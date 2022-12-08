165bbcf6ce71394cd4436f39d0d3cda309c9a2b6
rebykimani <beckireby15@gmail.com>
2022-12-09 01:29:03 +0300

    fix: node modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import SignUp from "./SignUp.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Hello from "./Hello";
-import Login from "./Login";
-import Home from "./Components/Home";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Hello from "./Hello";
+// import Login from "./Login";
+// import Home from "./Components/Home";
 import routes from "./routes";
 import { useRoutes } from "react-router-dom";
 import ThemeConfig from "./theme";
